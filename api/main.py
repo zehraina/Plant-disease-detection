aa3335d04cba973aff57816289580af810518db7
FANSAN6190 <fanin.s.pbl@gmail.com>
2023-10-03 16:27:30 +0530

    basic changes and testing

--- api/main.py
+++ api/main.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI, File, UploadFile
+import numpy as np
+from io import BytesIO
+from PIL import Image
 import uvicorn
 app = FastAPI()
 @app.get("/ping")
 async def ping():
     return "Hello FastAPI is alive"
 
+
+def read_file_as_image()->np.ndarray:
+    Image.open(BytesIO(data)) 
 @app.post("/predict")
 async def predict(
     file: UploadFile = File(...)
 ):
-    bytes=await
+    image=read_file_as_image(await file.read())
+    return
 
 if __name__=="__main__":
     uvicorn.run(app,host='localhost',port=8000)
