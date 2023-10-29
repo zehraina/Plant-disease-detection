1540e4e16c6c426c7d38b57ddeb8bb8631a1e0da
FANSAN6190 <fanin.s.pbl@gmail.com>
2023-10-29 16:03:46 +0530

    updated main.py

--- api/main.py
+++ api/main.py
@@ -8,20 +8,38 @@ import requests
 import cv2
 app = FastAPI()
 
-MODEL = tf.keras.models.load_model("../model_building_and_testing/models/saved_models/2/model2.pb")
+MODEL = tf.keras.models.load_model("../model_building_and_testing/models/saved_models/3/model3.pb")
 # endpoint = "http://localhost:8502/v1/models/potatoes_model:predict"
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+CLASS_NAMES2 = ["Early Blight", "Late Blight", "Healthy"]
+
+CLASS_NAMES3=['Potato : early blight',
+ 'Potato : healthy',
+ 'Potato : late blight',
+ 'Tomato : bacterial spot',
+ 'Tomato : early blight',
+ 'Tomato : healthy',
+ 'Tomato : late blight',
+ 'Tomato : leaf mold',
+ 'Tomato : septoria leaf spot',
+ 'Tomato : spider mites two-spotted spider mite',
+ 'Tomato : target spot',
+ 'Tomato : tomato mosaic virus',
+ 'Tomato : tomato yellow leaf curl virus',
+ 'Corn : cercospora leaf spot gray leaf spot',
+ 'Corn : common rust',
+ 'Corn : healthy',
+ 'Corn : northern leaf blight']
 
 
 # specifying end point
 @app.get("/ping")
 def ping():
-    return "Hello world"
+    return "This is Plant Leaf Disease Prediction API."
 
 
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
-    image=cv2.resize(image,(224, 224))
+    image=cv2.resize(image,(180, 180))
     return image
 
 
@@ -34,14 +52,18 @@ async def predict(
     img_batch = np.expand_dims(image, 0)
 
     
- 
     predictions = MODEL.predict(img_batch)
     
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    print(predicted_class)
+    predicted_class = np.argmax(predictions[0])
+    class_name=CLASS_NAMES3[predicted_class]
+    
+    print(class_name)   
     confidence = np.max(predictions[0])
     
-    return predicted_class, confidence
+    return {
+        'class': class_name,
+        'confidence': float(confidence)
+    }
     
 
     # predictions = MODEL.predict(img_batch)
