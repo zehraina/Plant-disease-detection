006c5f891d0bb2326968bdf1b365dd316ff621ab
FANSAN6190 <fanin.s.pbl@gmail.com>
2023-10-30 13:09:37 +0530

    fixed issue with confidence score

--- api/main.py
+++ api/main.py
@@ -8,7 +8,7 @@ import requests
 import cv2
 app = FastAPI()
 
-MODEL = tf.keras.models.load_model("../model_building_and_testing/models/saved_models/3/model3.pb")
+MODEL = tf.keras.models.load_model("../model_building_and_testing/models/saved_models/4/model4.pb")
 # endpoint = "http://localhost:8502/v1/models/potatoes_model:predict"
 CLASS_NAMES2 = ["Early Blight", "Late Blight", "Healthy"]
 
@@ -39,7 +39,7 @@ def ping():
   
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
-    image=cv2.resize(image,(180, 180))
+    image=cv2.resize(image,(256, 256))/255.0
     return image
 
   
@@ -64,16 +64,6 @@ async def predict(
         'class': class_name,
         'confidence': float(confidence)
     }
-    
-
-    # predictions = MODEL.predict(img_batch)
-    # predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    # confidence = np.max(predictions[0])
-    # return {
-    #     'class': predicted_class,
-    #     'confidence': float(confidence)
-    # }
-    # # pass
 
 
 if __name__ == "__main__":
