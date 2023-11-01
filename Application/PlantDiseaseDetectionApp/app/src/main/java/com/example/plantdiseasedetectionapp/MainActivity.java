01fbff660a6dd6db641ba4269859a0b95dcd0aa0
FANSAN6190 <fanin.s.pbl@gmail.com>
2023-11-01 21:04:55 +0530

    added make_prediction method to Predict button

--- Application/PlantDiseaseDetectionApp/app/src/main/java/com/example/plantdiseasedetectionapp/MainActivity.java
+++ Application/PlantDiseaseDetectionApp/app/src/main/java/com/example/plantdiseasedetectionapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -23,6 +24,7 @@ import android.graphics.Bitmap;
 import android.util.Base64;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -33,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     String encodedImage;
     byte[] byteArray;
     String prediction;
+
+    Bitmap img;
+    boolean image_received=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +66,72 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void make_prediction(View view){
+        if(image_received==false) {
+            Toast.makeText(this, "Please Select an Image First!!!.", Toast.LENGTH_SHORT).show();
+            Log.d("MainActivity", "make_prediction: Please Select an Image First!!!.");
+            return;
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // Need to CHANGE this URL repeatedly for different ngrok server instances
+                    URL url = new URL("https://fansan.pagekite.me/predict");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setDoOutput(true);
+
+                    String boundary = "----WebKitFormBoundary7MA4YWxkTrZu0gW"; // You can use any string here
+                    conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+                    DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+                    wr.writeBytes("--" + boundary + "\r\n");
+                    wr.writeBytes("Content-Disposition: form-data; name=\"file\"; filename=\"image.jpg\"\r\n");
+                    wr.writeBytes("Content-Type: image/jpeg\r\n\r\n");
+
+                    // Convert the bitmap to a byte array
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    img.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                    byte[] bitmapData = bos.toByteArray();
+
+                    // Write the byte array to the output stream
+                    wr.write(bitmapData);
+                    wr.writeBytes("\r\n--" + boundary + "--\r\n");
+                    wr.flush();
+                    wr.close();
+
+                    int responseCode = conn.getResponseCode();
+                    Log.d("MainActivity", "Response code: " + responseCode);
+
+                    // Read the response
+                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String inputLine;
+                    StringBuilder content = new StringBuilder();
+
+                    while ((inputLine = in.readLine()) != null) {
+                        content.append(inputLine);
+                    }
+
+                    // Close connections
+                    in.close();
+                    conn.disconnect();
+
+                    // Display a toast with the response
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            prediction =content.toString();
+                            Log.d("MainActivity", "Response: " + content.toString());
+                            Toast.makeText(MainActivity.this, "Response: " + prediction, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -69,74 +140,23 @@ public class MainActivity extends AppCompatActivity {
 
             if(requestCode==GALLERY_REQ_CODE){
                 //for gallery
-
-                imgCamera.setImageURI(data.getData());
+                Uri imageUri = data.getData();
+                try {
+                    img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    imgCamera.setImageBitmap(img);
+                    image_received=true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+
             if(requestCode==CAMERA_REQ_CODE){
                 //for camera
-
-                final Bitmap img = (Bitmap)(data.getExtras().get("data"));
+                img = (Bitmap)(data.getExtras().get("data"));
                 imgCamera.setImageBitmap(img);
-
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            // Need to CHANGE this URL repeatedly for different ngrok server instances
-                            URL url = new URL("https://5872-14-139-241-203.ngrok.io/predict");
-                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                            conn.setRequestMethod("POST");
-                            conn.setDoOutput(true);
-
-                            String boundary = "----WebKitFormBoundary7MA4YWxkTrZu0gW"; // You can use any string here
-                            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-
-                            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
-                            wr.writeBytes("--" + boundary + "\r\n");
-                            wr.writeBytes("Content-Disposition: form-data; name=\"file\"; filename=\"image.jpg\"\r\n");
-                            wr.writeBytes("Content-Type: image/jpeg\r\n\r\n");
-
-                            // Convert the bitmap to a byte array
-                            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                            img.compress(Bitmap.CompressFormat.JPEG, 100, bos);
-                            byte[] bitmapData = bos.toByteArray();
-
-                            // Write the byte array to the output stream
-                            wr.write(bitmapData);
-                            wr.writeBytes("\r\n--" + boundary + "--\r\n");
-                            wr.flush();
-                            wr.close();
-
-                            int responseCode = conn.getResponseCode();
-                            Log.d("MainActivity", "Response code: " + responseCode);
-
-                            // Read the response
-                            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                            String inputLine;
-                            StringBuilder content = new StringBuilder();
-
-                            while ((inputLine = in.readLine()) != null) {
-                                content.append(inputLine);
-                            }
-
-                            // Close connections
-                            in.close();
-                            conn.disconnect();
-
-                            // Display a toast with the response
-                            runOnUiThread(new Runnable() {
-                                public void run() {
-                                    prediction =content.toString();
-                                    Log.d("MainActivity", "Response: " + content.toString());
-                                    Toast.makeText(MainActivity.this, "Response: " + prediction, Toast.LENGTH_LONG).show();
-                                }
-                            });
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }).start();
+                image_received=true;
             }
+
         }
     }
 }
