c4c762b01bf9a2f00b6e70adfe4d93ff5f2fc868
Ishatvb <Ishatvbeohar22082003@gmail.com>
2023-11-09 03:22:34 +0530

    imgCamera and imgGallery

--- New_Application/Plant_Disease_detection/app/src/main/java/com/example/plant_disease_detection/ui/home/HomeFragment.java
+++ New_Application/Plant_Disease_detection/app/src/main/java/com/example/plant_disease_detection/ui/home/HomeFragment.java
@@ -18,6 +18,12 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private TextView textView1, textView5;
+    private int CAMERA_REQ_CODE;
+    private int GALLERY_REQ_CODE;
+    ImageView imgDisplay;
+    TextView result1, result2;
+
+
     private ImageView imageView1, imageView2, imageView3, imageView4, imageView5, imageView7, imageView8;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -28,7 +34,9 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
         // Initialize views
+        imgDisplay = findViewById(R.id.imgDisplay);
         textView1 = root.findViewById(R.id.textView1);
         textView5 = root.findViewById(R.id.textView5);
         imageView1 = root.findViewById(R.id.imageView1);
@@ -44,26 +52,36 @@ public class HomeFragment extends Fragment {
         imageView1.setOnClickListener(v -> {
             textView1.setText("APPLE");
             textView5.setText("Content for Image 1 - TextView3");
+            CAMERA_REQ_CODE = 10;
+            GALLERY_REQ_CODE = 20;
         });
 
         imageView2.setOnClickListener(v -> {
             textView1.setText("POTATO");
             textView5.setText("Content for Image 2 - TextView3");
+            CAMERA_REQ_CODE = 30;
+            GALLERY_REQ_CODE = 40;
         });
 
         imageView3.setOnClickListener(v -> {
             textView1.setText("CORN");
             textView5.setText("Content for Image 3 - TextView3");
+            CAMERA_REQ_CODE = 50;
+            GALLERY_REQ_CODE = 60;
         });
 
         imageView4.setOnClickListener(v -> {
             textView1.setText("TOMATO");
             textView5.setText("Content for Image 4 - TextView3");
+            CAMERA_REQ_CODE = 70;
+            GALLERY_REQ_CODE = 80;
         });
 
         imageView5.setOnClickListener(v -> {
             textView1.setText("RICE");
             textView5.setText("Content for Image 5 - TextView3");
+            CAMERA_REQ_CODE = 90;
+            GALLERY_REQ_CODE = 100;
         });
 
         final TextView textView = binding.textHome;
