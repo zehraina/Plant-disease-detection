439dd2a47689db9f7b35368547f117fca6b6ce9f
Ishatvb <Ishatvbeohar22082003@gmail.com>
2023-11-26 17:50:09 +0530

    Added all dependencies

--- New_Application/Plant_Disease_detection/app/src/main/java/com/example/plant_disease_detection/LoginActivity.java
+++ New_Application/Plant_Disease_detection/app/src/main/java/com/example/plant_disease_detection/LoginActivity.java
@@ -19,6 +19,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
     private EditText editTextLoginEmail, editTextLoginPassword;
     private ProgressBar progressBar;
--- New_Application/Plant_Disease_detection/app/src/main/java/com/example/plant_disease_detection/RegisterActivity.java
+++ New_Application/Plant_Disease_detection/app/src/main/java/com/example/plant_disease_detection/RegisterActivity.java
@@ -21,6 +21,18 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Calendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
